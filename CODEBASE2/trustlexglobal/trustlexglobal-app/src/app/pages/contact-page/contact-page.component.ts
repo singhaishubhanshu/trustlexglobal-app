ec165a66cd26c0aa561c6a44a54a5e051c8aef7f
Abhinav295 <abhinavjain295@gmail.com>
2026-06-29 03:28:21 +0530

    Enabling the mailing services

--- CODEBASE2/trustlexglobal/trustlexglobal-app/src/app/pages/contact-page/contact-page.component.ts
+++ CODEBASE2/trustlexglobal/trustlexglobal-app/src/app/pages/contact-page/contact-page.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { ScrollAnimationDirective } from '../../directives/scroll-animation.directive';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [CommonModule, ScrollAnimationDirective],
+  imports: [CommonModule, FormsModule,ScrollAnimationDirective],
   templateUrl: './contact-page.component.html',
   styleUrls: ['./contact-page.component.css','../../../styles.css'],
 })
 export class ContactPageComponent {
   // Replaces useState(false)
   submitted = false;
+  isLoading = false;
+  errorMessage ='';
+  formData = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    service: '',
+    goals: '',
+    time: ''
+  };
 
   services = [
     'USA LLC & Corporation Formation',
@@ -36,8 +48,29 @@ export class ContactPageComponent {
   ];
 
   // Replaces the const handleSubmit = (e) => {...} arrow function
-  onSubmit(event: Event) {
+  async onSubmit(event: Event) {
     event.preventDefault();
+    this.isLoading = true;
+    this.errorMessage='';
     this.submitted = true;
+
+    const backendURL = 'https://trustlexmailservice-305697903916.asia-south2.run.app'
+    try {
+      const response = await fetch(backendURL,{
+        method:'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(this.formData)
+      });
+      if (response.ok) {
+        this.submitted = true;
+      } else {
+        this.errorMessage = 'Something went wrong. Please try again.';
+      }
+    } catch (error) {
+      console.error('Email sending failed:', error);
+      this.errorMessage = 'Network error. Please check your connection.';
+    } finally {
+      this.isLoading = false;
+    }
   }
 }
